dbfa4db7e639803316db2725e15e31dea5e6eb29
acidcoke <hinzmann.o@gmail.com>
2021-10-06 13:31:28 +0200

    fixes #1

--- S3/objects.tf
+++ S3/objects.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.website.id
   key    = "index.html"
   source = "./website/index.html"
+  content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
