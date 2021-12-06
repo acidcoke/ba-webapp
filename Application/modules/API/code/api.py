a19c0d26b53fb4c67f04c6c2c291c2d9d6188ca9
acidcoke <hinzmann.o@gmail.com>
2021-12-06 22:30:06 +0100

    extract env access, logging-setup from handler

--- Application/modules/API/code/api.py
+++ Application/modules/API/code/api.py
@@ -8,43 +8,41 @@ import bson.json_util
 
 from pymongo import MongoClient
 
+SECRET_ARN = os.environ.get('SECRET_ARN')
+MONGO_BASE_URL = os.environ.get('MONGO_BASE_URL')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    SECRET_ARN = os.environ.get('SECRET_ARN')
-    MONGO_BASE_URL = os.environ.get('MONGO_BASE_URL')
-
-    ENTRIES_RESOURCE = '/entries'
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    secret = get_secret(SECRET_ARN)
-    uri = create_uri(json.loads(secret), MONGO_BASE_URL)
-    client = MongoClient(uri)
-    db = client.guestbook
-    entries = db.entries
     logger.info(f"EVENT: {event}")
-    if event['httpMethod'] == 'GET':
-        entry_cursor = entries.find()
-        entry_cursor_list = list(entry_cursor)
-        for cursor in entry_cursor_list:
-            del cursor['_id']
-        json_data = bson.json_util.dumps(entry_cursor_list)
-        return {
-            "statusCode": 202,
-            "body": json_data
-        }
-
-    elif event['httpMethod'] == 'POST':
-        entry = json.loads(event['body'])
-        entry["date"] = time.time()
-        entries.insert_one(entry)
-        return {
-            "statusCode": 201,
-            "headers": {
-                "Content-Type": "application/json"
+    if event['resource'] == '/entries':
+        secret = get_secret(SECRET_ARN)
+        uri = create_uri(json.loads(secret), MONGO_BASE_URL)
+        client = MongoClient(uri)
+        db = client.guestbook
+        entries = db.entries
+        if event['httpMethod'] == 'GET':
+            entry_cursor = entries.find()
+            entry_cursor_list = list(entry_cursor)
+            for cursor in entry_cursor_list:
+                del cursor['_id']
+            json_data = bson.json_util.dumps(entry_cursor_list)
+            return {
+                "statusCode": 202,
+                "body": json_data
             }
-        }
+
+        elif event['httpMethod'] == 'POST':
+            entry = json.loads(event['body'])
+            entry["date"] = time.time()
+            entries.insert_one(entry)
+            return {
+                "statusCode": 201,
+                "headers": {
+                    "Content-Type": "application/json"
+                }
+            }
 
 
 def get_secret(secret_arn):
