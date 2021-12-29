e3fb680b5842c3a910e0102754d045a998826b82
acidcoke <hinzmann.o@gmail.com>
2021-12-29 16:01:22 +0100

    rename DB to Database

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ module "API" {
 
   aws_region  = var.aws_region
   name_prefix = var.name_prefix
-  vpc_id      = module.DB.vpc_id
+  vpc_id      = module.Database.vpc_id
 
-  mongodb_ingress_hostname = module.DB.mongodb_ingress_hostname
-  mongodb_secret           = module.DB.mongodb_secret
+  mongodb_ingress_hostname = module.Database.mongodb_ingress_hostname
+  mongodb_secret           = module.Database.mongodb_secret
 
   website_bucket_name = var.website_bucket_name
 }
@@ -22,8 +22,8 @@ module "Frontend" {
   website_bucket_name = var.website_bucket_name
 }
 
-module "DB" {
-  source = "./modules/DB"
+module "Database" {
+  source = "./modules/Database"
 
   aws_region  = var.aws_region
   name_prefix = var.name_prefix
--- modules/Frontend/outputs.tf
+++ modules/Frontend/outputs.tf
@@ -1,11 +1,3 @@
-output "website" {
-  value = aws_s3_bucket.website.website
-}
-
-output "website_domain" {
-  value = aws_s3_bucket.website.website_domain
-}
-
 output "website_endpoint" {
   value = aws_s3_bucket.website.website_endpoint
 }
